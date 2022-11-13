0c22a736f45985aa7692aa6a1a64cc9f8f53361d
Dang Duc Hieu <ddhieu3103@gmail.com>
2022-11-13 09:48:34 +0700

    onlythuyetmang

---
+++ mang/lythuyetvemang.py
@@ -0,0 +1,18 @@
+diemsohocsinh = [8,9,7,8,10]
+
+#lay phan tu trong mang ra
+print(diemsohocsinh[0])
+print(diemsohocsinh[2])
+
+#in ra mang truoc khi them 2 diem vao
+for diem in diemsohocsinh:
+    print("Diem so hoc sinh la", diem)
+
+#them phan tu vao mang
+diemsohocsinh.append(2)
+
+
+#in ra mang sau khi them 2 diem vao
+for diem in diemsohocsinh:
+    print("Diem sau khi them 2 vao", diem)
+
