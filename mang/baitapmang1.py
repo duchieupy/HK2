0eea3b9b348aa8a34e69a494f99695a8061a7d8c
Dang Duc Hieu <ddhieu3103@gmail.com>
2022-11-13 10:21:05 +0700

    lam xong bai tinh diem chan

---
+++ mang/baitapmang1.py
@@ -0,0 +1,9 @@
+mangdiemhocsinh = [5,8,7,9,10,4,2,1]
+
+#in ra cac diem hoc sinh nho hon 5 trong mang
+
+#chay qua truoc -> coi ong nao nho hon 5 thi in
+for ong in mangdiemhocsinh :
+    if ong  < 5 :
+        print("day la diem nho hon 5",ong)
+    
---
+++ mang/baitapmang2.py
@@ -0,0 +1,17 @@
+mangdiemhocsinh = [5,8,7,9,10,4,2,1]
+
+#dem xem mang nay co bao nhieu diem chan
+sodiemchan=0 
+
+for diem in mangdiemhocsinh :
+    if diem % 2 == 0:     
+        sodiemchan=sodiemchan + 1
+        
+print("so diem chan",sodiemchan)
+
+
+    
+        
+        
+       
+       
