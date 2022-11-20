d4047a21b011d7ebe6a02c68daede00b263371f6
Dang Duc Hieu <ddhieu3103@gmail.com>
2022-11-20 10:32:09 +0700

    ly thuyet mang

---
+++ mang-20-11-2022/lythuyetvemang.py
@@ -0,0 +1,11 @@
+mangdiemhocsinh = [1,3,6,10]
+tongsophantu =0
+
+#moi lan chay qua thi phantu cong them 1
+for chay in mangdiemhocsinh :  
+    tongsophantu=tongsophantu+1
+
+for vitri in range(tongsophantu):
+    print("vi tri thu: ", vitri)
+    print("gia tri cua phan tu la: ", mangdiemhocsinh[vitri])
+    print("--------------------")
