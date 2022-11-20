1e03e6b9d25bb414c5e831733c68eadc1b805578
Dang Duc Hieu <ddhieu3103@gmail.com>
2022-11-20 10:32:54 +0700

    tim so lon nhat va vi tri

---
+++ mang-20-11-2022/timsolonnhatvavitri.py
@@ -0,0 +1,20 @@
+mangdiemhocsinh = [1,3,6, 10, 9, 8, 4, 2, 7]
+
+#tim so lon nhat trong mang
+
+tongsophantu =0
+
+#moi lan chay qua thi phantu cong them 1
+for chay in mangdiemhocsinh :  
+    tongsophantu=tongsophantu+1
+
+solonnhat = mangdiemhocsinh[0]
+vitrisolonnhat = 0
+
+for vitri in range(tongsophantu):
+    if solonnhat < mangdiemhocsinh[vitri]:
+        solonnhat = mangdiemhocsinh[vitri]
+        vitrisolonnhat = vitri
+        
+print("so lon nhat", solonnhat)
+print("vi tri so lon nhat", vitrisolonnhat)
