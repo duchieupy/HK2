d1681178a2463cea0c7f5a02e53da6982d7510e6
Dang Duc Hieu <ddhieu3103@gmail.com>
2022-12-05 11:00:36 +0700

    các cách lam mảng

---
+++ buoi5-04122022/lythuyetmang.py
@@ -0,0 +1,15 @@
+# co 2 cach lap qua mang
+#c1
+
+diemsohocsinh = [8,9,7,8,10]
+for diem in diemsohocsinh:
+    print("Diem so hoc sinh la", diem)
+
+#cach 2
+mangten = ["danh", "hieu", "hoang", "trung"]
+
+for vitri, ten in enumerate(mangten):
+    print("vi tri thu: ", vitri)
+    print("ten la: ", ten)
+    print("in ten theo kieu vi tri la", mangten[vitri])#mangten[1] => hieu
+    
