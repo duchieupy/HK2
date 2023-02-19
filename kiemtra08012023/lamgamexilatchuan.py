8f4ecbf6e3a0e059da380d01427efb3ecf5a4667
Dang Duc Hieu <ddhieu3103@gmail.com>
2023-02-19 15:16:52 +0700

    bai xi lat xong truong hop tinh xi dach + xi vang

--- kiemtra08012023/lamgamexilatchuan.py
+++ kiemtra08012023/lamgamexilatchuan.py
@@ -1,6 +1,6 @@
 #cho mang chua cac quan bai
-quanbaiHieu = ["A","A"]
-quanbaiCau = ["A","A"]
+quanbaiHieu = ["A","Q"]
+quanbaiCau = ["A","K"]
 
 #tinh diem tat ca quan bai cua nguoi choi tra ve diem
 def tinhdiemtoanboquanbai(toanboquanbai):
@@ -10,10 +10,12 @@ def tinhdiemtoanboquanbai(toanboquanbai):
         case 2:
             #xi vang
             if toanboquanbai == ["A","A"]:
-                return 9999
+                return 53
             #xi lat
-            
+            if (toanboquanbai == ["A","J"] ) or (toanboquanbai == ["A","K"] )or (toanboquanbai == ["A","Q"]) or (toanboquanbai == ["A","10"]):
+                return 52
             #binh thuong
+            
                 return "sap win"
         case 3:
             return 3
@@ -28,5 +30,27 @@ diemcuacau = tinhdiemtoanboquanbai(quanbaiCau)
 print("diem cua Hieu la", diemcuaHieu)
 print("diem cua Cau la", diemcuacau)
 
-if diemcuaHieu == diemcuacau:
-    print("hoa")
+
+# hoa
+# khi diem bang nhau, hoac 2 ong woac het
+if (diemcuaHieu == diemcuacau) or (diemcuacau  and diemcuaHieu >21 and diemcuacau  and diemcuaHieu <50  ) :
+    print("hoa") 
+# cau thang
+# diemcau > diemhieu va cau khong woac
+else:
+    if (diemcuacau > diemcuaHieu) and (diemcuacau  < 22 or diemcuacau>50):
+        print("cau thang con")
+
+# hieu thang
+    if (diemcuaHieu > diemcuacau) and (diemcuaHieu  < 22 or diemcuaHieu>50):
+        print("con thang cau")
+
+
+
+# quy dinh
+# xi vang 53
+# xi lat 52
+# ngu linh 51
+# woac thi lon hon 21 va nho hon 50
+# woac thi la 22,23,24,....50 => ko woac=25
+# 
